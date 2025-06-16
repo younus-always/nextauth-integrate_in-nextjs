2b1264a189014724d71d8f930c17bdc1be7eeaa1
younus-always <younusofficial443@gmail.com>
2025-06-16 23:44:10 +0600

    mongodb database connect and register form create client component

---
+++ src/app/register/components/RegisterForm.jsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { RegisterUser } from "@/app/actions/auth/RegisterUser";
+
+const RegisterForm = () => {
+      const handleRegister = async (e) => {
+            e.preventDefault();
+            const form = e.target;
+            const username = form.username.value;
+            // const email = form.email.value;
+            const password = form.password.value;
+            const user = { username, password };
+            //
+            const result = await RegisterUser(user);
+            console.log(result)
+      };
+
+      return (
+            <div className="max-w-lg mx-auto mt-12 p-8 bg-slate-300 rounded-lg">
+                  <h2 className="text-center text-2xl font-semibold py-4">Register your account</h2>
+                  <form onSubmit={handleRegister} className="space-y-3">
+                        <div>
+                              <label htmlFor="username" className="block mb-2">Username:</label>
+                              <input type="text" name="username" id="username" placeholder="username" className="p-2 rounded-lg bg-slate-100 text-sm w-full ring-1 focus:outline-0" />
+                        </div>
+                        <div>
+                              <label htmlFor="password" className="block mb-2">Password:</label>
+                              <input type="password" name="password" id="password" placeholder="password" className="p-2 rounded-lg bg-slate-100 text-sm w-full ring-1 focus:outline-0" />
+                        </div>
+                        <button type="submit" className="w-full p-2 rounded-lg bg-indigo-600 font-semibold text-slate-50 mt-4 cursor-pointer active:scale-95 duration-150">Register</button>
+                  </form>
+            </div>
+      )
+}
+
+export default RegisterForm
---
+++ src/lib/dbConnect.js
@@ -0,0 +1,15 @@
+import { MongoClient, ServerApiVersion } from "mongodb"
+
+const uri = process.env.MONGO_URI;
+
+export const dbConnect = (collectionName) => {
+      // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+      const client = new MongoClient(uri, {
+            serverApi: {
+                  version: ServerApiVersion.v1,
+                  strict: true,
+                  deprecationErrors: true,
+            }
+      });
+      return client.db(process.env.DB_NAME).collection(collectionName);
+};
